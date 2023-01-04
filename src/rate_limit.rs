d51fee64f2fc6ba6656f84d8934e4502fa7d8afc
novacrazy <novacrazy@gmail.com>
2023-01-03 23:57:53 -0600

    Future-proof rate-limiting to use Result

--- src/rate_limit.rs
+++ src/rate_limit.rs
@@ -6,6 +6,11 @@ pub struct RateLimiter {
     pub last: Instant,
 }
 
+#[derive(Debug)]
+pub struct RateLimitError {
+    pub limit: f32,
+}
+
 impl Default for RateLimiter {
     fn default() -> Self {
         RateLimiter {
@@ -19,20 +24,20 @@ use crate::Route;
 
 impl RateLimiter {
     /// Update this limiter. Will return true if within limits.
-    pub fn update<S>(&mut self, route: &Route<S>, req_per_sec: f32) -> bool {
-        // get the number of decayed requests since the last request
-        let decayed = route.start.duration_since(self.last).as_millis() as f32 * req_per_sec * 0.001;
+    pub fn update<S>(&mut self, route: &Route<S>, req_per_sec: f32) -> Result<(), RateLimitError> {
+        // get the number of "decayed" requests since the last request
+        let decayed = route.start.duration_since(self.last).as_millis() as f32 * (req_per_sec * 0.001);
         // compute the effective number of requests performed
         let req_count = self.count - decayed;
 
-        let within_limit = req_count < req_per_sec;
-
-        if within_limit {
+        if req_count < req_per_sec {
             // update with new request
             self.count = req_count.max(0.0) + 1.0;
             self.last = route.start;
-        }
 
-        within_limit
+            Ok(())
+        } else {
+            Err(RateLimitError { limit: req_per_sec })
+        }
     }
 }
