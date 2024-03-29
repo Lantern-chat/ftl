1f77552ff1c5ea4865c77beede763ac53900bdd3
novacrazy <novacrazy@gmail.com>
2024-03-29 16:15:32 -0500

    Check more headers for real_ip

--- src/real_ip.rs
+++ src/real_ip.rs
@@ -1,7 +1,7 @@
 use std::net::{AddrParseError, IpAddr};
 use std::str::FromStr;
 
-use http::header::ToStrError;
+use http::header::{HeaderName, ToStrError};
 
 use super::*;
 
@@ -17,17 +17,28 @@ pub enum GetRealIpError {
     AddrParseError(#[from] AddrParseError),
 }
 
+/// Attempts to get the real IP address of the client using a variety of headers.
 pub fn get_real_ip<S>(route: &Route<S>) -> Result<IpAddr, GetRealIpError> {
     let headers = route.headers();
 
     if let Some(Ok(mut proxies)) = route.forwarded_for() {
-        if let Some(first) = proxies.next() {
-            return Ok(first?);
+        // NOTE: If it cannot be parsed, it will be ignored.
+        if let Some(Ok(first)) = proxies.next() {
+            return Ok(first);
         }
     }
 
-    if let Some(x_real_ip) = headers.get("x-real-ip") {
-        return Ok(IpAddr::from_str(x_real_ip.to_str()?.trim())?);
+    static HEADERS: [HeaderName; 4] = [
+        HeaderName::from_static("x-real-ip"),
+        HeaderName::from_static("client-ip"), // used by some load balancers
+        HeaderName::from_static("x-cluster-client-ip"), // used by AWS sometimes
+        HeaderName::from_static("cf-connecting-ip"), // used by Cloudflare sometimes
+    ];
+
+    for header in &HEADERS {
+        if let Some(x_forwarded_for) = headers.get(header) {
+            return Ok(IpAddr::from_str(x_forwarded_for.to_str()?.trim())?);
+        }
     }
 
     Ok(route.addr.ip())
